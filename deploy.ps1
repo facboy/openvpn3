e4ea05e965fb12a101c1e8f91b481b2b80dc652a
Christopher Ng <facboy@gmail.com>
2023-04-18 10:22:55 +0100

    Update deploy script for new build layout

--- deploy.ps1
+++ deploy.ps1
@@ -1,32 +1,11 @@
-#at top of script
-if (!
-    #current role
-    (New-Object Security.Principal.WindowsPrincipal(
-        [Security.Principal.WindowsIdentity]::GetCurrent()
-    #is admin?
-    )).IsInRole(
-        [Security.Principal.WindowsBuiltInRole]::Administrator
-    )
-) {
-    $cwd = [Environment]::CurrentDirectory
-    #elevate script and exit current non-elevated runtime
-    Start-Process `
-        -FilePath 'powershell' `
-        -ArgumentList (
-            #flatten to single array
-            '-File', $MyInvocation.MyCommand.Source, $args `
-            | %{ $_ }
-        ) `
-        -WorkingDirectory $cwd `
-        -Verb RunAs `
-        -Wait
-    exit
-}
+gsudo {
+	sc.exe stop ovpnagent
 
-sc.exe stop ovpnagent
-cd $PSScriptRoot
-robocopy "build\openvpn\omi\Release\" "c:\bin\openvpn3\omi" /w:5
-robocopy "build\openvpn\ovpnagent\win\Release\" "c:\bin\openvpn3\ovpnagent" /w:5
-robocopy "build\test\ovpncli\Release\" "c:\bin\openvpn3\ovpncli" /w:5
-sc.exe start ovpnagent
-pause
+	cd $args[0]
+
+	robocopy "build\msvc\amd64\openvpn\omi\Release\\" "c:\bin\openvpn3\omi" /w:5
+	robocopy "build\msvc\amd64\openvpn\ovpnagent\win\Release\" "c:\bin\openvpn3\ovpnagent" /w:5
+	robocopy "build\msvc\amd64\test\ovpncli\Release\" "c:\bin\openvpn3\ovpncli" /w:5
+
+	sc.exe start ovpnagent
+} -args $PSScriptRoot
