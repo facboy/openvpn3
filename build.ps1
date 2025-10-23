616ced19cba08698802041de0ccc1e6833fea1c4
Christopher Ng <facboy@gmail.com>
2025-10-23 22:00:58 +0100

    Fix build for latest vcpkg

--- build.ps1
+++ build.ps1
@@ -1,20 +1,21 @@
 # prepare
-#set VCPKG_ROOT="D:\git\vcpkg"
+#$env:VCPKG_ROOT = "D:\git\vcpkg"
+$cmake = "c:\Program Files\CMake\bin\cmake.exe"
 
 $vcpkg_manifest_dir = Join-Path "$PSScriptRoot" "deps\vcpkg_manifests\windows"
 Set-Location "$vcpkg_manifest_dir"
-vcpkg x-update-baseline
+& 'd:\git\vcpkg\vcpkg' --vcpkg-root="d:\git\vcpkg" x-update-baseline
 Set-Location "$PSScriptRoot"
 
 $parallel = 15
 
-cmake --preset win-x64-release
+& "$cmake" --preset my-release
 
 # build targets
-cmake --build --preset win-x64-release --target ovpnagent --parallel "$parallel"
+& "$cmake" --build --preset win-x64-release --target ovpnagent --parallel "$parallel"
 
-cmake --build --preset win-x64-release --target omicliagent --parallel "$parallel"
-cmake --build --preset win-x64-release --target omicli --parallel "$parallel"
+& "$cmake" --build --preset win-x64-release --target omicliagent --parallel "$parallel"
+& "$cmake" --build --preset win-x64-release --target omicli --parallel "$parallel"
 
-cmake --build --preset win-x64-release --target ovpncliagent --parallel "$parallel"
-cmake --build --preset win-x64-release --target ovpncli --parallel "$parallel"
+& "$cmake" --build --preset win-x64-release --target ovpncliagent --parallel "$parallel"
+& "$cmake" --build --preset win-x64-release --target ovpncli --parallel "$parallel"
